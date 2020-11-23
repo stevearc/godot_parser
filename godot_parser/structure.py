8162d88ed0b807c753a732b83fe894b23e0b8d80
Jose Juan Montes <jjmontes@gmail.com>
2020-11-23 07:58:24 +0100

    Supports quoted keys in resources.

--- godot_parser/structure.py
+++ godot_parser/structure.py
@@ -4,6 +4,7 @@ from pyparsing import (
     Empty,
     Group,
     Optional,
+    QuotedString,
     Suppress,
     Word,
     alphanums,
@@ -14,7 +15,10 @@ from pyparsing import (
 from .sections import GDSection, GDSectionHeader
 from .values import value
 
-key = Word(alphanums + "_/").setName("key")
+key = (
+    QuotedString('"', escChar="\\", multiline=False).setName("key") 
+    | Word(alphanums + "_/").setName("key")
+)
 var = Word(alphanums + "_").setName("variable")
 attribute = Group(var + Suppress("=") + value)
 
