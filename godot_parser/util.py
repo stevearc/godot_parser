3f7f75995cfa757c4d5e97d020005e74426f0702
Bruno Meneguello <bruno@meneguello.com>
2025-06-24 19:04:58 -0300

    feat: adds-support to typed arrays and string names

--- godot_parser/util.py
+++ godot_parser/util.py
@@ -10,19 +10,19 @@ def stringify_object(value):
     if value is None:
         return "null"
     elif isinstance(value, str):
-        return json.dumps(value)
+        return json.dumps(value, ensure_ascii=False)
     elif isinstance(value, bool):
         return "true" if value else "false"
     elif isinstance(value, dict):
         return (
             "{\n"
             + ",\n".join(
-                ['"%s": %s' % (k, stringify_object(v)) for k, v in value.items()]
+                ['%s: %s' % (json.dumps(k, ensure_ascii=False) if isinstance(k, str) else str(k), stringify_object(v)) for k, v in value.items()]
             )
             + "\n}"
         )
     elif isinstance(value, list):
-        return "[ " + ", ".join([stringify_object(v) for v in value]) + " ]"
+        return "[" + ", ".join([stringify_object(v) for v in value]) + "]"
     else:
         return str(value)
 
